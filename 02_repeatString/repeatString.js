ef719fe90b190056f4227b3b2df4ef4353cf29c7
Joshua Zhu <jzhu2003@gmail.com>
2025-04-18 14:56:44 -0700

    finished exercises 1-7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let output = "";
+    if (num < 0){
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++){
+        output = output + string;
+    }
+    return output;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
+    if ((!Number.isInteger(min) || !Number.isInteger(max)|| max < 0 || min < 0)){
+        return "ERROR";
+    }
+    if (min > max){
+        temp = max;
+        max = min;
+        min = temp;
+    }
+    let sum = 0;
+    for (let i = min; i <= max; i++){
+        sum += i;
+    }
+    return sum;
 
 };
 
